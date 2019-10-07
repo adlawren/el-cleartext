e7dbcd282f720f36eab9c5ea23afe8a99c73688f
A.L <adlawren010@gmail.com>
2019-10-07 15:21:46 -0600

    Highlight uncommon words

--- main.el
+++ main.el
@@ -1,15 +1,22 @@
-;; TODO:
-;; - Make it highlight words which are NOT in the list
-;; - Rewrite; it's too slow
-;;   - It's currently linear with respect to the number of words in the common_words file
-;;   - Make it linear with respect to the number of words in the buffer
+;; TODO
+;; - It will break if not run in a file which is the same directory as common_words.txt
+;; - Should be more efficient
+;;   - Right now, it's iterating over each word in the buffer, and (potentially) doing a regex search for the word
+;;     - It would be better if the text properties of each word could be updated without needing to do a regex search
 
-(defun highlight-common-words ()
+(defun highlight-uncommon-words ()
   (interactive)
+  (setq common-words-hash (make-hash-table))
   (mapcar
-   (lambda (word) (highlight-regexp word))
+   (lambda (word)
+     (puthash (intern word) "t" common-words-hash))
    (split-string
     (with-temp-buffer
       (insert-file-contents "common_words.txt")
-      (buffer-substring-no-properties (point-min) (point-max)))
-    "\n")))
+      (buffer-substring-no-properties (point-min) (point-max)))))
+  (mapcar
+   (lambda (word)
+     (if (not (gethash (intern (downcase word)) common-words-hash))
+         (highlight-regexp word)))
+   (split-string
+    (buffer-substring-no-properties (point-min) (point-max)))))
